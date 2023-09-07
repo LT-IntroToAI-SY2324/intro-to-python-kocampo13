b3e141b4171906fca71de9395e694f89f8246e90
Kelly Ocampo <kocampo13@cps.edu>
2023-09-07 10:37:28 -0500

    Guess the Number almost complete

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,9 +1,22 @@
 import random
 
- person_guess = 3
- computer_pick = 10
+
+person_guess = 4
+computer_pick = 10
 
 def guess_number():
-   num_pick = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10]
-   person_guess = 4
-   computer_pick = random.guess(num_pick)
+   pick = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10]
+   person_guess = 6
+   computer_pick = random.choice(pick)
+   guess = {"Your guess": person_guess, "Compuer's pick": computer_pick}
+
+   return guess
+
+guess = guess_number()
+print(guess)
+if person_guess == computer_pick:
+   print("Correct!")
+
+else:
+   print("Incorrct guess. Try again")
+
